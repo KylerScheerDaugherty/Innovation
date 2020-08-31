7317951f36d7fbe7a3261e1d838c833e0bff92ae
Kyler Scheer <Kyler.Scheer@daugherty.com>
2020-08-31 10:37:36 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Scheer, Kyler (STL) on 8/31/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/InnovationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnovationApp.swift
+//  Shared
+//
+//  Created by Scheer, Kyler (STL) on 8/31/20.
+//
+
+import SwiftUI
+
+@main
+struct InnovationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
